b3b218924973aa05989f9256396d3775a447ff17
Jessica Bassard <53714967+jbassard88@users.noreply.github.com>
2019-11-21 15:44:43 -0800

    compare choices

---
+++ script.js
@@ -0,0 +1,70 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+        });
+    };
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach(option => {
+            option.addEventListener('click', function() {
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+                playerHand.src = `./icons/${this.textContent}.png`;
+                computerHand.src = `./icons/${computerChoice}.png`;
+            });
+        });
+    };
+    const compareHands = (playerChoice, computerChoice) => {
+        const winner = document.querySelector('.winner');
+        if(playerChoice === computerChoice){
+            winner.textContent = 'It is a tie';
+            return;
+        };
+        if(playerChoice === 'rock'){
+            if(computerChoice === 'scissors'){
+                winner.textContent = 'Player Wins';
+                return;
+            }else{
+                winner.textContent = 'Computer Wins';
+                return;
+            }
+        }
+        if(playerChoice === 'paper'){
+            if(computerChoice === 'rock'){
+                winner.textContent = 'Player Wins';
+                return;
+            }else{
+                winner.textContent = 'Computer Wins';
+                return;
+            }
+        }
+        if(playerChoice === 'scissors'){
+            if(computerChoice === 'paper'){
+                winner.textContent = 'Player Wins';
+                return;
+            }else{
+                winner.textContent = 'Computer Wins';
+                return;
+            }
+        }
+    };
+
+
+    startGame();
+    playMatch();
+};
+
+game();
