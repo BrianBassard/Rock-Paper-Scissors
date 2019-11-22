10a7713d645ca9aade4bb6a3f9385e6fb22ce3f5
MrBlackTie <35544593+BrianBassard@users.noreply.github.com>
2019-11-22 15:34:58 -0800

    Function Complete
    Basic functionof app complete.

--- script.js
+++ script.js
@@ -16,20 +16,46 @@ const game = () => {
         const options = document.querySelectorAll('.options button');
         const playerHand = document.querySelector('.player-hand');
         const computerHand = document.querySelector('.computer-hand');
+
+        const hands = document.querySelectorAll('.hands img');
+
+        hands.forEach(hand =>{
+            hand.addEventListener('animationend', function (){
+                this.style.animation = '';
+            })
+        })
+
         const computerOptions = ['rock', 'paper', 'scissors'];
 
         options.forEach(option => {
             option.addEventListener('click', function() {
+
+                playerHand.src = `./icons/rock.png`;
+                computerHand.src = `./icons/rock.png`;
+
                 const computerNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[computerNumber];
-
                 //Computer and Player Hands
 
-                playerHand.src = `./icons/${this.textContent}.png`;
-                computerHand.src = `./icons/${computerChoice}.png`;
+                setTimeout(() =>{
+                    compareHands(this.textContent, computerChoice);
+
+                    playerHand.src = `./icons/${this.textContent}.png`;
+                    computerHand.src = `./icons/${computerChoice}.png`;
+                }, 2000);
+
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease";
             });
         });
     };
+
+    const updateScore  = () =>{
+        const playerScore = document.querySelector('.player-score p');
+        const computerScore = document.querySelector('.computer-score p');
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    }
     const compareHands = (playerChoice, computerChoice) => {
         const winner = document.querySelector('.winner');
         if(playerChoice === computerChoice){
@@ -42,27 +68,39 @@ const game = () => {
         if(playerChoice === 'rock'){
             if(computerChoice === 'scissors'){
                 winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
                 return;
             }else{
                 winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
                 return;
             }
         }
         if(playerChoice === 'paper'){
             if(computerChoice === 'rock'){
                 winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
                 return;
             }else{
                 winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
                 return;
             }
         }
         if(playerChoice === 'scissors'){
             if(computerChoice === 'paper'){
                 winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
                 return;
             }else{
                 winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
                 return;
             }
         }
